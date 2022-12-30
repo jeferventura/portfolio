d89d78397af364d2b64da0f22900db2656b4bf28
Jeferson Ventura <jefersonventura91@gmail.com>
2022-12-29 21:59:11 -0300

    inicialização do projeto e instalação de bibliotecas do react

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,33 +1,26 @@
-import React from 'react';
+import React from "react";
 
+import { Agencia, Hotel, Clinica, Upconsultoria } from "./Jobs/carousel";
 
-import { 
-    Agencia, 
-    Hotel, 
-    Clinica, 
-    Upconsultoria
-}  from './Jobs/carousel'
-
-import './assets/css/style.min.css';
+//import "./assets/css/style.min.css";
 
 const Portfolio = () => {
-    return (
-        <section className="portfolio">
-            <div className="portfolio-container">
-                <div className="container">
-                    <h2 className="section-title">Portfólio</h2>
-                    <div className="portfolio-carousel scroll-bar-ajustes">
-                        <Agencia />
-                        <Hotel />
-                        <Clinica />
-                        <Upconsultoria />
-                    </div>
-                </div>
-            </div>
-            <div className="format-bg"></div>
-        </section>
-    );
-    
-}
+  return (
+    <section className="portfolio">
+      <div className="portfolio-container">
+        <div className="container">
+          <h2 className="section-title">Portfólio</h2>
+          <div className="portfolio-carousel scroll-bar-ajustes">
+            <Agencia />
+            <Hotel />
+            <Clinica />
+            <Upconsultoria />
+          </div>
+        </div>
+      </div>
+      <div className="format-bg"></div>
+    </section>
+  );
+};
 
 export default Portfolio;
